f4710a2931afcb6d5d5a17be69cefe13b0d87770
Subash <subasssh27@gmail.com>
2026-02-28 22:59:02 +0530

    twilio added

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,45 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import os
+from twilio.rest import Client
 
 app = Flask(__name__)
 CORS(app)
 
-# store latest data
 latest_data = {
     "fire": False,
     "temperature": 0
 }
 
-# ESP32 will send data here
+def send_sms(message):
+    account_sid = os.environ.get("TWILIO_SID")
+    auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+    twilio_number = os.environ.get("TWILIO_PHONE")
+
+    client = Client(account_sid, auth_token)
+
+    client.messages.create(
+        body=message,
+        from_=twilio_number,
+        to="+919363025728"
+    )
+
 @app.route("/update", methods=["POST"])
 def update():
     global latest_data
     data = request.json
-    latest_data["fire"] = data.get("fire", False)
-    latest_data["temperature"] = data.get("temperature", 0)
+
+    fire = data.get("fire", False)
+    temperature = data.get("temperature", 0)
+
+    latest_data["fire"] = fire
+    latest_data["temperature"] = temperature
+
+    if fire:
+        send_sms(f"🔥 FIRE ALERT! Temperature: {temperature}°C")
+
     return jsonify({"message": "Data updated"}), 200
 
-# Website reads data from here
 @app.route("/status")
 def status():
     return jsonify(latest_data)
